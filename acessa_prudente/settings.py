facd212ea26c91731adacbd09961da0583c75f63
Emerson <acessaprudente@gmail.com>
2022-06-11 23:27:44 +0000

    settings

--- acessa_prudente/settings.py
+++ acessa_prudente/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ml*b94x&=ad=^ne0flgodfzt75zss5c4dny$r82+$axb3a&bf@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['acessa.pythonanywhere.com', '127.0.0.1', 'localhost']
 
